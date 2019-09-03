3c63b147ec412515a492dc4bc1d56c59259c95eb
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2019-09-03 12:06:51 +0200

    util/meson_aux/extra-dist-cmd.sh: Use MESON_DIST_ROOT

--- util/meson_aux/extra-dist-cmd.sh
+++ util/meson_aux/extra-dist-cmd.sh
@@ -2,17 +2,16 @@
 
 # External command, intended to be called with meson.add_dist_script() in meson.build
 
-# extra-dist-cmd.sh <root_source_dir> <root_build_dir> <relative_dist_dir>
+# extra-dist-cmd.sh <root_source_dir> <root_build_dir>
 
-# <relative_dist_dir> is the distribution directory path relative to <root_build_dir>.
 # Meson does not preserve timestamps on distributed files.
 # But this script preserves the timestamps on libstdc++.tag.
 
 # Make a ChangeLog file for distribution.
 git --git-dir="$1/.git" --work-tree="$1" log --no-merges --date=short --max-count=200 \
-    --pretty='tformat:%cd  %an  <%ae>%n%n  %s%n%w(0,0,2)%+b' > "$2/$3/ChangeLog"
+    --pretty='tformat:%cd  %an  <%ae>%n%n  %s%n%w(0,0,2)%+b' > "$MESON_DIST_ROOT/ChangeLog"
 
 # Distribute the libstdc++.tag file in addition to the files in the local git clone.
 # -p == --preserve=mode,ownership,timestamps (Posix does not support long options.)
 # Only the preservation of timestamps is essential here.
-cp -p "$2/libstdc++.tag" "$2/$3/doctags/"
+cp -p "$2/libstdc++.tag" "$MESON_DIST_ROOT/doctags/"
