90dd879e36952831964e2d25d36168d4b0e789cd
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2025-12-16 15:51:59 +0100

    Meson build: Allow -Duse-network=if-no-local-tag-file
    This is use-network's new default value.
    libstdc++.tag is downloaded only if it's necessary. Typically only
    the first time mm-common is built in a new build directory.
    As with use-network=false, the tag file is copied from the source
    directory if it exists there, e.g. in an expanded tarball.
    
    See the last part of Discourse comment
    https://discourse.gnome.org/t/glibmm-windows-rm-is-not-recognised-as-an-internal-or-external-command/32916/12

--- util/meson_aux/libstdcxx-tag.py
+++ util/meson_aux/libstdcxx-tag.py
@@ -2,17 +2,18 @@
 
 # External command, intended to be called with custom_target() in meson.build
 
-#                         argv[1]         argv[2]    argv[3]
-# libstdcxx-tag.py <curl-or-wget-or-none> <srcdir> <output_path>
+#                       argv[1]             argv[2]          argv[3]    argv[4]
+# libstdcxx-tag.py <use_network_opt> <curl-or-wget-or-none> <srcdir> <output_path>
 
 import os
 import sys
 import subprocess
 import shutil
 
-subcommand = sys.argv[1]
-srcdir = sys.argv[2]
-output_path = sys.argv[3]
+use_network_opt = sys.argv[1]
+subcommand = sys.argv[2]
+srcdir = sys.argv[3]
+output_path = sys.argv[4]
 output_dirname, output_filename = os.path.split(output_path)
 if not output_dirname:
   output_dirname = '.'
@@ -61,26 +62,45 @@ def wget():
     ]
   return subprocess.run(cmd).returncode
 
-def dont_download_tag_file():
+def download_if_possible():
+  # use_network_opt == 'true' or 'if-no-local-tag-file'
+  subcommand_base = os.path.splitext(os.path.basename(os.path.normpath(subcommand)))[0]
+  if subcommand_base == 'curl':
+    return curl()
+  if subcommand_base == 'wget':
+    return wget()
+
+  if not os.path.isfile(output_path):
+    print('Error:', output_path, 'does not exist.', file=sys.stderr)
+    print('Downloading it is not possible because no download command exists.',
+          file=sys.stderr)
+  else:
+    print('Error: Downloading', output_path,
+          'is not possible because no download command exists.', file=sys.stderr)
+  print('Please install "curl" or "wget".', file=sys.stderr)
+  return 1
+
+def copy_or_download():
+  # use_network_opt == 'false' or 'if-no-local-tag-file'
   if os.path.isfile(output_path):
     print('Did not check status of', output_path, 'because network is disabled.')
   elif os.path.isfile(os.path.join(srcdir, output_filename)):
-    print('Warning:', output_path, 'does not exist.')
-    print('Copying from the source directory because network is disabled.')
+    print('Warning:', output_path, 'does not exist in the build directory.')
+    print('Copying from the source directory.')
     print('If you want an up-to-date copy, reconfigure with the -Duse-network=true option.')
     # shutil.copy2() copies timestamps and some other file metadata.
     shutil.copy2(os.path.join(srcdir, output_filename), output_path)
-  else:
+  elif use_network_opt == 'if-no-local-tag-file':
+    return download_if_possible()
+  else: # use_network_opt == 'false'
     print('Error:', output_path, 'does not exist.', file=sys.stderr)
     print('Downloading it is not possible because network is disabled.', file=sys.stderr)
-    print('Please reconfigure with the -Duse-network=true option.', file=sys.stderr)
+    print('Please reconfigure with the -Duse-network=true or',
+          '-Duse-network=if-no-local-tag-file option.', file=sys.stderr)
     return 1
   return 0
 
 # ----- Main -----
-subcommand_base = os.path.splitext(os.path.basename(os.path.normpath(subcommand)))[0]
-if subcommand_base == 'curl':
-  sys.exit(curl())
-if subcommand_base == 'wget':
-  sys.exit(wget())
-sys.exit(dont_download_tag_file())
+if use_network_opt == 'true':
+  sys.exit(download_if_possible())
+sys.exit(copy_or_download())
